5d8154ccd588fa67bd3aca3aa237dcf3fd4b6720
Łukasz Kliś <lukasz@klis.pl>
2012-01-06 18:32:33 +0100

    Init repo

---
+++ README.md
@@ -0,0 +1,10 @@
+# Logs Manager
+
+A simple tool which allows you easily to archive your gaming server's logs (but not only).
+
+## Usage
+
+    logs = LogsManager.new("filename", "host", "user", "pass", "directory")
+    logs.archive
+
+__Please note:__ _filename_ should be provided without an extension.
---
+++ logs_manager.rb
@@ -0,0 +1,21 @@
+require "net/ftp"
+require "date"
+
+class LogsManager
+  
+  def initialize(file_name, url, user, pass, dir)
+    @f = file_name
+    @ftp = Net::FTP.open(url)
+    @ftp.login(user, pass)
+    @ftp.chdir(dir)
+  end
+
+  def archive
+    @ftp.rename("#{@f}.log", "#{@f}-#{(Date.today - 1).to_s}-#{(Date.today).to_s}.log")
+    @ftp.close
+  end
+
+end
+
+logs = LogsManager.new("server", "host", "user", "pass", "directory")
+logs.archive
